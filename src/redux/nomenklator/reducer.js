bc5a503282077272a54ef5b082bf38e6d81e480f
Kolodiva <Kolodiva@mail.ru>
2019-10-23 19:06:03 +0300

    yo

--- src/redux/nomenklator/reducer.js
+++ src/redux/nomenklator/reducer.js
@@ -4,6 +4,7 @@ module.exports = function(state, action) {
   state = state || {
     value: 0,
     recs: [],
+    ahtung_postgre:'',
   };
 
   switch (action.type) {
@@ -15,11 +16,20 @@ module.exports = function(state, action) {
       return {
         value: state.value - 1
       };
+
+      case 'PUT_AXIOS_AHTUNG':
+        //console.log( action.payload );
+        return {
+          ahtung_postgre: 'action.payload',
+        };
+
     case 'GET_AXIOS':
       //console.log( action.payload );
       return {
         value:  state.value - 1,
         recs:   action.payload,
+
+        ahtung_postgre: '',
       };
 
     default:
