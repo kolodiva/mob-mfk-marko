fbeb9b71731516e4657c48bc48b575ff91fcb9df
Kolodiva <Kolodiva@mail.ru>
2019-11-13 16:05:05 +0300

    yo

--- src/routes.js
+++ src/routes.js
@@ -85,7 +85,7 @@ exports.sendEmail = (req, res) => {
 //to: 'kolodiva@gmail.com, kolodiva@mail.ru, gl-@list.ru, adv.mfc@newfurnitura.ru' ,
   sendmail({
     from: 'adv@newfurnitura.ru',
-    to: 'kolodiva@gmail.com, kolodiva@mail.ru, gl-@list.ru, adv.mfc@gmail.com' ,
+    to: 'kolodiva@gmail.com, kolodiva@mail.ru, gl-@list.ru, adv.mfc@gmail.com, gleblesenko.77@yandex.ru' ,
     subject: 'Информация о понижении уровня цен.',
     html: '<img class="" style="width: 12vw" src="https://www.newfurnitura.ru/upload/mailing/logo_big_orig.png"/><h4 style="color: blue">Наконец рады сообщить, что уровень цен опустился ниже нуля.<br/>Да здравствует ясновидец Глеб и все его планы... и весь 1 кабинет пусть будет в здравии.<h4><img class="" style="width: 12vw" src="https://newfurnitura.ru/upload/af19f6a1-aaba-4778-a943-ba9a0665.png"/><br/><a href="https://newfurnitura.ru">На базу</a>'
   }, function (err, reply) {
