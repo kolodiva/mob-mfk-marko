d163aa5014481db8d0cfc6313fd06ad294ec829e
Kolodiva <Kolodiva@mail.ru>
2019-11-14 11:29:47 +0300

    yo

--- src/routes.js
+++ src/routes.js
@@ -93,7 +93,7 @@ exports.sendEmail = (req, res) => {
 //to: 'kolodiva@gmail.com, kolodiva@mail.ru, gl-@list.ru, adv.mfc@newfurnitura.ru' ,
   sendmail({
     from: 'mfc@newfurnitura.ru',
-    to: 'kolodiva@mail.ru, kolodiva@gmail.com, gl-@list.ru, adv.mfc@gmail.com',
+    to: 'kolodiva@mail.ru, kolodiva@gmail.com, gl-@list.ru, adv.mfc@gmail.com,  mebel_furnitura@hotmail.com',
     subject: 'Анонс. Международная мебельная выставка Мосбилд-2019 на Красной Пресне. Ура.',
     html: '<img class="" style="width: 12vw" src="https://www.newfurnitura.ru/upload/mailing/logo_big_orig.png"/><h4 style="">С удовольствие приглашаем Вас посетить наш стенд.<br/>Детали во вложении.<h4><img class="" style="width: 12vw" src="https://newfurnitura.ru/upload/af19f6a1-aaba-4778-a943-ba9a0665.png"/><br/><a target="_blank" href="https://www.newfurnitura.ru/news/Unihopper_08_2019.pdf_prefix_xEsgkzRXVojgIjOjN_Dg1w.pdf">Послание Якобинцам от Глеба.</a><br/><br/><a  target="_blank" href="https://www.newfurnitura.ru/unscribe_email?email=kolodiva@mail.ru&code=c4ca4238a0b923820dcc509a6f75849b">Не желаю боле получать Вашу рассылку. Не нравится она мне.</a>',
     attachments: [
