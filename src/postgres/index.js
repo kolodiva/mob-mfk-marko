34985ed60a9724ac05d7e1ad708437d23b25bd62
Kolodiva <Kolodiva@mail.ru>
2019-11-18 12:17:25 +0300

    ok

--- src/postgres/index.js
+++ src/postgres/index.js
@@ -214,6 +214,17 @@ const mailAction = (db, sendmail) => {
 							    subject: rec0.description,
 									html: strHtml,
 							  })
+							).then(
+
+								function (err, reply) {
+
+										if (err && err.stack) {
+												//console.log( email[0], err.stack )
+												db.query( `update mailing_lists set subscribed=false where email=${email[0]}` ).then( (res) => { return ' -- ' + email[0] + ' not exist and unscribed.' });
+										}
+								    // console.dir( reply )
+								  }
+
 							);
 
 							// try {
