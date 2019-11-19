3db52856e4b875c34362698238b17586917ef865
Kolodiva <Kolodiva@mail.ru>
2019-11-19 08:42:41 +0300

    ok

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ router.get( '/',                    routes.getHome );
 router.get( '/catalog/:guidParent', routes.getCatalog );
 router.get( '/catalog',             routes.getCatalog );
 
-router.get( '/exhibition',          routes.getexhibition );
+router.get( '/exhibition',          routes.getExhibition );
 
 router.get( '/filial_info/:id',     routes.getContacts );
 router.get( '/filial_info',         routes.getContacts );
